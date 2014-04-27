18b5e3e13728a4bdde754ea109b368752ce06e86
Patrick Hogan <pbhogan@gmail.com>
2014-04-27 18:27:57 -0500

    Added InterfaceMovement example.

---
+++ Assets/InControl/Examples/InterfaceMovement/ButtonFocus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace InterfaceMovement
+{
+	public class ButtonFocus : MonoBehaviour
+	{
+		void Update()
+		{
+			// Get focused button.
+			var focusedButton = transform.parent.GetComponent<ButtonManager>().focusedButton;
+
+			// Move toward same position as focused button.
+			transform.position = Vector3.MoveTowards( transform.position, focusedButton.transform.position, Time.deltaTime * 10.0f );
+		}
+	}
+}
