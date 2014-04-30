47b84240aed4edc21930451fc69bde858ffdecbf
Patrick Hogan <pbhogan@gmail.com>
2014-04-29 19:55:48 -0500

    Bump.

--- Assets/InControl/Library/VersionInfo.cs
+++ Assets/InControl/Library/VersionInfo.cs
@@ -8,7 +8,7 @@ namespace InControl
 		public const int Major = 1;
 		public const int Minor = 1;
 		public const int Patch = 2;
-		public const int Build = 1740;
+		public const int Build = 1759;
 
 
 		public override string ToString()
