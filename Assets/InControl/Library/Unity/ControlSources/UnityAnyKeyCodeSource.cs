e7fdc5a5a4762b031a2efba1bbf6b24376c67302
Greg Harding <greg@flightless.co.nz>
2014-07-11 22:39:43 +1200

    Refactored key combination inputs.

--- Assets/InControl/Library/Unity/ControlSources/UnityAnyKeyCodeSource.cs
+++ Assets/InControl/Library/Unity/ControlSources/UnityAnyKeyCodeSource.cs
@@ -6,8 +6,8 @@ namespace InControl
 {
 	public class UnityAnyTwoKeyCodeSource : InputControlSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
+		protected KeyCode keyCode1;
+		protected KeyCode keyCode2;
 		
 		
 		public UnityAnyTwoKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2 )
@@ -30,90 +30,54 @@ namespace InControl
 	}
 
 
-	public class UnityAnyThreeKeyCodeSource : InputControlSource
+	public class UnityAnyThreeKeyCodeSource : UnityAnyTwoKeyCodeSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
-		KeyCode keyCode3;
+		protected KeyCode keyCode3;
 		
 		
-		public UnityAnyThreeKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3 )
+		public UnityAnyThreeKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3 ) : base ( keyCode1, keyCode2 )
 		{
-			this.keyCode1 = keyCode1;
-			this.keyCode2 = keyCode2;
 			this.keyCode3 = keyCode3;
 		}
+
 		
-		
-		public float GetValue( InputDevice inputDevice )
-		{
-			return GetState( inputDevice ) ? 1.0f : 0.0f;
-		}
-		
-		
-		public bool GetState( InputDevice inputDevice )
+		public new bool GetState( InputDevice inputDevice )
 		{
 			return Input.GetKey( keyCode1 ) || Input.GetKey( keyCode2 ) || Input.GetKey( keyCode3 );
 		}
 	}
 
 
-	public class UnityAnyFourKeyCodeSource : InputControlSource
+	public class UnityAnyFourKeyCodeSource : UnityAnyThreeKeyCodeSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
-		KeyCode keyCode3;
-		KeyCode keyCode4;
+		protected KeyCode keyCode4;
 		
 		
-		public UnityAnyFourKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4 )
+		public UnityAnyFourKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4 ) : base ( keyCode1, keyCode2, keyCode3 )
 		{
-			this.keyCode1 = keyCode1;
-			this.keyCode2 = keyCode2;
-			this.keyCode3 = keyCode3;
 			this.keyCode4 = keyCode4;
 		}
 		
-		
-		public float GetValue( InputDevice inputDevice )
-		{
-			return GetState( inputDevice ) ? 1.0f : 0.0f;
-		}
-		
-		
-		public bool GetState( InputDevice inputDevice )
+
+		public new bool GetState( InputDevice inputDevice )
 		{
 			return Input.GetKey( keyCode1 ) || Input.GetKey( keyCode2 ) || Input.GetKey( keyCode3 ) || Input.GetKey( keyCode4 );
 		}
 	}
 
 
-	public class UnityAnyFiveKeyCodeSource : InputControlSource
+	public class UnityAnyFiveKeyCodeSource : UnityAnyFourKeyCodeSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
-		KeyCode keyCode3;
-		KeyCode keyCode4;
-		KeyCode keyCode5;
+		protected KeyCode keyCode5;
 
 
-		public UnityAnyFiveKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4, KeyCode keyCode5 )
+		public UnityAnyFiveKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4, KeyCode keyCode5 ) : base ( keyCode1, keyCode2, keyCode3, keyCode4 )
 		{
-			this.keyCode1 = keyCode1;
-			this.keyCode2 = keyCode2;
-			this.keyCode3 = keyCode3;
-			this.keyCode4 = keyCode4;
 			this.keyCode5 = keyCode5;
 		}
 		
-		
-		public float GetValue( InputDevice inputDevice )
-		{
-			return GetState( inputDevice ) ? 1.0f : 0.0f;
-		}
-		
-		
-		public bool GetState( InputDevice inputDevice )
+
+		public new bool GetState( InputDevice inputDevice )
 		{
 			return Input.GetKey( keyCode1 ) || Input.GetKey( keyCode2 ) || Input.GetKey( keyCode3 ) || Input.GetKey( keyCode4 ) || Input.GetKey( keyCode5 );
 		}
--- Assets/InControl/Library/Unity/ControlSources/UnitySimultaneousKeyCodeSource.cs
+++ Assets/InControl/Library/Unity/ControlSources/UnitySimultaneousKeyCodeSource.cs
@@ -6,8 +6,8 @@ namespace InControl
 {
 	public class UnitySimultaneousTwoKeyCodeSource : InputControlSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
+		protected KeyCode keyCode1;
+		protected KeyCode keyCode2;
 		
 		
 		public UnitySimultaneousTwoKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2 )
@@ -28,92 +28,56 @@ namespace InControl
 			return Input.GetKey( keyCode1 ) && Input.GetKey( keyCode2 );
 		}
 	}
-
-
-	public class UnitySimultaneousThreeKeyCodeSource : InputControlSource
+	
+	
+	public class UnitySimultaneousThreeKeyCodeSource : UnitySimultaneousTwoKeyCodeSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
-		KeyCode keyCode3;
+		protected KeyCode keyCode3;
 		
 		
-		public UnitySimultaneousThreeKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3 )
+		public UnitySimultaneousThreeKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3 ) : base ( keyCode1, keyCode2 )
 		{
-			this.keyCode1 = keyCode1;
-			this.keyCode2 = keyCode2;
 			this.keyCode3 = keyCode3;
 		}
 		
 		
-		public float GetValue( InputDevice inputDevice )
-		{
-			return GetState( inputDevice ) ? 1.0f : 0.0f;
-		}
-		
-		
-		public bool GetState( InputDevice inputDevice )
+		public new bool GetState( InputDevice inputDevice )
 		{
 			return Input.GetKey( keyCode1 ) && Input.GetKey( keyCode2 ) && Input.GetKey( keyCode3 );
 		}
 	}
-
-
-	public class UnitySimultaneousFourKeyCodeSource : InputControlSource
+	
+	
+	public class UnitySimultaneousFourKeyCodeSource : UnitySimultaneousThreeKeyCodeSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
-		KeyCode keyCode3;
-		KeyCode keyCode4;
+		protected KeyCode keyCode4;
 		
 		
-		public UnitySimultaneousFourKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4 )
+		public UnitySimultaneousFourKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4 ) : base ( keyCode1, keyCode2, keyCode3 )
 		{
-			this.keyCode1 = keyCode1;
-			this.keyCode2 = keyCode2;
-			this.keyCode3 = keyCode3;
 			this.keyCode4 = keyCode4;
 		}
 		
 		
-		public float GetValue( InputDevice inputDevice )
-		{
-			return GetState( inputDevice ) ? 1.0f : 0.0f;
-		}
-		
-		
-		public bool GetState( InputDevice inputDevice )
+		public new bool GetState( InputDevice inputDevice )
 		{
 			return Input.GetKey( keyCode1 ) && Input.GetKey( keyCode2 ) && Input.GetKey( keyCode3 ) && Input.GetKey( keyCode4 );
 		}
 	}
-
-
-	public class UnitySimultaneousFiveKeyCodeSource : InputControlSource
+	
+	
+	public class UnitySimultaneousFiveKeyCodeSource : UnitySimultaneousFourKeyCodeSource
 	{
-		KeyCode keyCode1;
-		KeyCode keyCode2;
-		KeyCode keyCode3;
-		KeyCode keyCode4;
-		KeyCode keyCode5;
-
-
-		public UnitySimultaneousFiveKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4, KeyCode keyCode5 )
+		protected KeyCode keyCode5;
+		
+		
+		public UnitySimultaneousFiveKeyCodeSource( KeyCode keyCode1, KeyCode keyCode2, KeyCode keyCode3, KeyCode keyCode4, KeyCode keyCode5 ) : base ( keyCode1, keyCode2, keyCode3, keyCode4 )
 		{
-			this.keyCode1 = keyCode1;
-			this.keyCode2 = keyCode2;
-			this.keyCode3 = keyCode3;
-			this.keyCode4 = keyCode4;
 			this.keyCode5 = keyCode5;
 		}
 		
 		
-		public float GetValue( InputDevice inputDevice )
-		{
-			return GetState( inputDevice ) ? 1.0f : 0.0f;
-		}
-		
-		
-		public bool GetState( InputDevice inputDevice )
+		public new bool GetState( InputDevice inputDevice )
 		{
 			return Input.GetKey( keyCode1 ) && Input.GetKey( keyCode2 ) && Input.GetKey( keyCode3 ) && Input.GetKey( keyCode4 ) && Input.GetKey( keyCode5 );
 		}
