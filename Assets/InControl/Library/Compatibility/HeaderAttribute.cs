4993a2a9c64293768413c203b0f6ccd606a43ad4
Patrick Hogan <patrick@gallantgames.com>
2014-07-27 16:57:37 -0500

    Added space, header and range property attributes for Unity 4.3 compatibility.

---
+++ Assets/InControl/Library/Compatibility/HeaderAttribute.cs
@@ -0,0 +1,45 @@
+﻿#if UNITY_4_3 && UNITY_EDITOR
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace InControl
+{
+	public class HeaderAttribute : PropertyAttribute
+	{
+		public string header;
+
+		public HeaderAttribute( string header )
+		{
+			this.header = header;
+		}
+	}
+
+
+	[CustomPropertyDrawer(typeof(HeaderAttribute))]
+	public class HeaderDrawer : PropertyDrawer
+	{
+		public override float GetPropertyHeight( SerializedProperty property, GUIContent label )
+		{
+			return EditorGUI.GetPropertyHeight( property, label ) + (EditorGUIUtility.singleLineHeight * 1.5f);
+		}
+		
+		
+		public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+		{
+			var headerAttribute = attribute as HeaderAttribute;
+			var propertyHeight = position.height - (EditorGUIUtility.singleLineHeight * 1.5f);
+
+			position.y += EditorGUIUtility.singleLineHeight * 0.5f;
+			position.height = EditorGUIUtility.singleLineHeight;
+			EditorGUI.LabelField( position, headerAttribute.header, EditorStyles.boldLabel );
+
+			position.y += EditorGUIUtility.singleLineHeight;
+			position.height = propertyHeight;
+			EditorGUI.PropertyField( position, property );
+		}
+	}
+}
+#endif
+
