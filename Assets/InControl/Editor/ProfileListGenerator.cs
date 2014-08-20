b0c3796ba7b3fab5ebad8e4e24dd236543a2f246
Patrick Hogan <pbhogan@gmail.com>
2014-08-19 20:56:56 -0500

    Make the profile list generator content test ignore whitespace #153

--- Assets/InControl/Editor/ProfileListGenerator.cs
+++ Assets/InControl/Editor/ProfileListGenerator.cs
@@ -55,12 +55,11 @@ namespace InControl
 		{
 ";
 
-			string code3 = 
-@"		};
+			string code3 = @"		};
 	}
 }";
 
-			var code = Regex.Replace( code1 + code2 + code3, @"\r\n?|\n", Environment.NewLine );
+			var code = FixNewLines( code1 + code2 + code3 );
 			if (PutFileContents( filePath, code ))
 			{
 				Debug.Log( "InControl has updated the autodiscover profiles list." );
@@ -70,7 +69,7 @@ namespace InControl
 
 		static string GetFileContents( string fileName )
 		{			
-			StreamReader streamReader = new StreamReader( fileName );;			
+			StreamReader streamReader = new StreamReader( fileName );
 			var fileContents = streamReader.ReadToEnd();
 			streamReader.Close();
 			
@@ -81,7 +80,7 @@ namespace InControl
 		static bool PutFileContents( string filePath, string content )
 		{
 			var oldContent = GetFileContents( filePath );
-			if (content == oldContent)
+			if (CompareIgnoringWhitespace( content, oldContent ))
 			{
 				return false;
 			}
@@ -92,6 +91,18 @@ namespace InControl
 			streamWriter.Close();
 
 			return true;
+		}
+
+
+		static string FixNewLines( string text )
+		{
+			return Regex.Replace( text, @"\r\n|\n", Environment.NewLine );
+		}
+
+
+		static bool CompareIgnoringWhitespace( string s1, string s2 )
+		{
+			return Regex.Replace( s1, @"\s", "" ) == Regex.Replace( s2, @"\s", "" );
 		}
 	}
 }
