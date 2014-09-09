ab17f3b869402a4e6881645cc675fd2dc6572e2d
Arturas Slajus <arturas@tinylabproductions.com>
2014-09-09 11:59:21 +0300

    Tabs to spaces.

--- Assets/InControl/Library/Unity/DeviceProfiles/AndroidTvTouchInputProfile.cs
+++ Assets/InControl/Library/Unity/DeviceProfiles/AndroidTvTouchInputProfile.cs
@@ -20,45 +20,45 @@
       LowerDeadZone = 0.2f;
 
       ButtonMappings = new[] {
-  			new InputControlMapping {
-  				Handle = "A",
-  				Target = InputControlType.Action1,
-  				Source = Button0
-  			}
-  		};
+        new InputControlMapping {
+          Handle = "A",
+          Target = InputControlType.Action1,
+          Source = Button0
+        }
+      };
 
       AnalogMappings = new[] {
-				new InputControlMapping {
-					Handle = "DPad Left",
-					Target = InputControlType.DPadLeft,
-					Source = Analog4,
-					SourceRange = InputControlMapping.Range.Negative,
-					TargetRange = InputControlMapping.Range.Negative,
-					Invert = true
-				},
-				new InputControlMapping {
-					Handle = "DPad Right",
-					Target = InputControlType.DPadRight,
-					Source = Analog4,
-					SourceRange = InputControlMapping.Range.Positive,
-					TargetRange = InputControlMapping.Range.Positive
-				},
-				new InputControlMapping {
-					Handle = "DPad Up",
-					Target = InputControlType.DPadUp,
-					Source = Analog5,
-					SourceRange = InputControlMapping.Range.Negative,
-					TargetRange = InputControlMapping.Range.Negative
-				},
-				new InputControlMapping {
-					Handle = "DPad Down",
-					Target = InputControlType.DPadDown,
-					Source = Analog5,
-					SourceRange = InputControlMapping.Range.Positive,
-					TargetRange = InputControlMapping.Range.Positive,
-					Invert = true
-				},
-  		};
+        new InputControlMapping {
+          Handle = "DPad Left",
+          Target = InputControlType.DPadLeft,
+          Source = Analog4,
+          SourceRange = InputControlMapping.Range.Negative,
+          TargetRange = InputControlMapping.Range.Negative,
+          Invert = true
+        },
+        new InputControlMapping {
+          Handle = "DPad Right",
+          Target = InputControlType.DPadRight,
+          Source = Analog4,
+          SourceRange = InputControlMapping.Range.Positive,
+          TargetRange = InputControlMapping.Range.Positive
+        },
+        new InputControlMapping {
+          Handle = "DPad Up",
+          Target = InputControlType.DPadUp,
+          Source = Analog5,
+          SourceRange = InputControlMapping.Range.Negative,
+          TargetRange = InputControlMapping.Range.Negative
+        },
+        new InputControlMapping {
+          Handle = "DPad Down",
+          Target = InputControlType.DPadDown,
+          Source = Analog5,
+          SourceRange = InputControlMapping.Range.Positive,
+          TargetRange = InputControlMapping.Range.Positive,
+          Invert = true
+        },
+      };
     }
   }
 }
