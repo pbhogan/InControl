1d81a3725135c93e116e5d242ae5a698a6e88a14
Patrick Hogan <pbhogan@gmail.com>
2014-12-23 19:07:28 -0600

    Added Google Nexus Player profiles

---
+++ Assets/InControl/Source/Unity/DeviceProfiles/GoogleNexusPlayerRemoteProfile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace InControl
+{
+	[AutoDiscover]
+	public class GoogleNexusPlayerRemoteProfile : UnityInputDeviceProfile
+	{
+		public GoogleNexusPlayerRemoteProfile()
+		{
+			Name = "Google Nexus Player Remote";
+			Meta = "Google Nexus Player Remote";
+
+			SupportedPlatforms = new[] {
+				"Android"
+			};
+
+			JoystickNames = new[] {
+				"Google Nexus Remote"
+			};
+
+			ButtonMappings = new[] {
+				new InputControlMapping {
+					Handle = "A",
+					Target = InputControlType.Action1,
+					Source = Button0
+				},
+				new InputControlMapping {
+					Handle = "Back",
+					Target = InputControlType.Back,
+					Source = KeyCodeButton( UnityEngine.KeyCode.Escape )
+				}
+			};
+
+			AnalogMappings = new[] {
+				new InputControlMapping {
+					Handle = "DPad Left",
+					Target = InputControlType.DPadLeft,
+					Source = Analog4,
+					SourceRange = InputControlMapping.Range.Negative,
+					TargetRange = InputControlMapping.Range.Negative,
+					Invert = true
+				},
+				new InputControlMapping {
+					Handle = "DPad Right",
+					Target = InputControlType.DPadRight,
+					Source = Analog4,
+					SourceRange = InputControlMapping.Range.Positive,
+					TargetRange = InputControlMapping.Range.Positive
+				},
+				new InputControlMapping {
+					Handle = "DPad Up",
+					Target = InputControlType.DPadUp,
+					Source = Analog5,
+					SourceRange = InputControlMapping.Range.Negative,
+					TargetRange = InputControlMapping.Range.Negative,
+					Invert = true
+				},
+				new InputControlMapping {
+					Handle = "DPad Down",
+					Target = InputControlType.DPadDown,
+					Source = Analog5,
+					SourceRange = InputControlMapping.Range.Positive,
+					TargetRange = InputControlMapping.Range.Positive
+				},
+			};
+		}
+	}
+}
