c6755cdfe4e372e06112e4322d6a0467e07a3fd9
Cukia <cukiakimani@gmail.com>
2015-10-30 10:10:10 +0200

    added join screen

---
+++ Assets/InControl/Examples/JoinScreen/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using InControl;
+
+namespace JoinScreenExample
+{
+    public class GameManager : MonoBehaviour
+    {
+        public List<InputDevice> Controllers = new List<InputDevice>();
+
+        private static GameManager _instance;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<GameManager>();
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+                return _instance;
+            }
+        }
+
+        void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (this != _instance)
+                    Destroy(gameObject);
+            }
+        }
+    
+        void Update()
+        {
+        	if (Input.GetKeyDown(KeyCode.Alpha1))
+        		Application.LoadLevel(1);
+
+        	if (Input.GetKeyDown(KeyCode.Alpha2))
+        		Application.LoadLevel(2);
+        }
+    }
+}
---
+++ Assets/InControl/Examples/JoinScreen/JoinControllers.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using System.Linq;
+using UnityEngine.UI;
+
+namespace JoinScreenExample
+{
+    public class JoinControllers : MonoBehaviour
+    {
+
+        public Button PlayMatchButton;
+        public Toggle[] PlayerToggles;
+        
+        private int assignedPlayers;
+
+        void Update()
+        {
+            Join();
+
+            foreach (var a in GameManager.Instance.Controllers)
+            {
+                var control = a.GetControl(InputControlType.Start);
+
+                if (control.IsPressed && PlayMatchButton.IsInteractable())
+                    PlayMatch();
+            }
+        }
+
+        void Join()
+        {
+            foreach (var d in InputManager.Devices)
+            {
+                if (d.Action1.WasPressed)
+                {
+                    switch (assignedPlayers)
+                    {
+                        case 0:
+                            GameManager.Instance.Controllers.Add(d);
+                            print(PlayerToggles[assignedPlayers]);
+                            PlayerToggles[assignedPlayers].isOn = true;
+                            assignedPlayers++;
+                            PlayMatchButton.interactable = true;
+                            break;
+
+                        default:
+                            var inList = GameManager.Instance.Controllers.Any(c => c == d);
+                            if (!inList)
+                            {
+                                GameManager.Instance.Controllers.Add(d);
+                                PlayerToggles[assignedPlayers].isOn = true;
+                                assignedPlayers++;
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void PlayMatch()
+        {
+            Application.LoadLevel(2);
+        }
+
+    }
+}
